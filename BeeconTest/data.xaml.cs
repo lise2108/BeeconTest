1a6d68b4f4e64766736b3cfbd91a6e1aa099366d
Lise-Lu Yin Aaen <lise2108@edu.eal.dk>
2019-01-31 11:58:58 +0100

    Add project files.

---
+++ BeeconTest/data.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace BeeconTest
+{
+    /// <summary>
+    /// Interaction logic for data.xaml
+    /// </summary>
+    public partial class data : Window
+    {
+        public data()
+        {
+            InitializeComponent();
+        }
+    }
+}
